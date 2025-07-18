79dc7e614a9a73d8ff314b05348f43722d50e75a
MovinduSenura <movindusenuraaluthge@gmail.com>
2025-07-18 19:32:01 +0530

    font error fixed

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const Baumans = localFont({
+  src: "./fonts/Baumans-Regular.ttf",
+  variable: "--font-baumans",
 });
 
 export const metadata = {
@@ -19,9 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${Baumans.variable} antialiased`}>
         {children}
       </body>
     </html>
